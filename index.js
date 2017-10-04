5a8da5c31ed4e3ea59bb4427cd4fe7b861c36f16
Orin Fletcher <orin.fletcher@gmail.com>
2017-10-04 21:53:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
 function theBeatlesPlay(musicians, instruments){
   let beats = []
   for (var i = 0; i < musicians.length; i++) {
-    for (var j = 0; j < instruments.length; j++) {
-      beats.push(musicians[i] + " plays " + instruments[j])
-    }
+      beats.push(musicians[i] + " plays " + instruments[i])
   }
   console.log(beats)
   return beats
