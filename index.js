312a96320ccd36078199b354d89b1e1ce60d03f6
Orin Fletcher <orin.fletcher@gmail.com>
2017-10-04 21:44:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 function theBeatlesPlay(musicians, instruments){
   let beats = []
   for (var i = 0; i < musicians.length; i++) {
-    for (var j = i; j < instruments.length, j++){
-      console.log(musicians[i]+instruments[j]);
+    for (var j = i; j < instruments.length, j++) {
+      console.log(musicians[i]+instruments[j])
     }
   }
 }
 theBeatlesPlay(["John","Ringo","Paul", "George"],["Guitar","Bass","Drums"])
-
